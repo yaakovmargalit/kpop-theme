35eca601d2cb83d8ec836c63d2eed0516d41fc7e
Yaakov Margalit <yaakovmargalit@gmail.com>
2023-06-16 19:23:28 +0300

    first commit

---
+++ front-page.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        img {
+            width: 741px;
+            height: 477px;
+        }
+
+        h1 {
+            width: fit-content;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>היי ברוכים הבאים</h1>
+    <img src="https://www.trvl.co.il/wp-content/uploads/2017/02/ea3cb3092efd1c3e81584d04ee44408be273ead31db1104893f2_640_view.jpg"
+        alt="">
+</body>
+
+</html>
